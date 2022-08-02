617714e39f08ea5c5ce4c6068b8f0cc0f99a3cf9
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-08-02 11:10:32 +0300

    created interfaces for Data access objects

---
+++ src/main/java/models/dao/StaffDao.java
@@ -0,0 +1,22 @@
+package models.dao;
+
+import models.Staff;
+
+import java.util.List;
+
+public interface StaffDao {
+    //create
+    void add(Staff staff);
+    //void addStaffToDepartment(Staff staff, Department department);
+
+    //read
+    List<Staff> getAll();
+    // List<Staff>getAllStaff(int id);
+
+    //update
+    //omit for now
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
+}
