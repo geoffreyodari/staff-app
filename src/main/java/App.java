fe684f705008bb87af3b88a5b9a20e66b5f40074
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-08-04 13:00:06 +0300

    added gson dependancy

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,35 @@
+import models.Department;
+import models.dao.Sql2oDepartmentDao;
+import models.dao.Sql2oStaffDao;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static spark.Spark.*;
+
 public class App {
+
+
+    public static void main(String[] args) {
+        Sql2oStaffDao staffDao;
+        Sql2oDepartmentDao departmentDao;
+        Gson gson = new Gson();
+        Connection conn;
+
+        String connectionString = "jdbc:h2:~/staffapp.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        staffDao = new Sql2oStaffDao(sql2o);
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        conn = sql2o.open();
+
+        //CREATE
+        post("/departments/new", "application/json", (req, res) -> {
+            Department department = gson.fromJson(req.body(), Department.class);
+            departmentDao.add(department);
+            res.status(201);;
+            return gson.toJson(department);
+        });
+
+
+
+    }
 }
